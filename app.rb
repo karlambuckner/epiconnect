0933a99740c2d47ef728aa4020efa03c8a9c6a6e
Gabrijela Gavric, Karla Buckner, Sarah Lindmar and Sara Spink <sarah@>
2017-10-02 15:19:29 -0700

    adjust styling and begin routing

--- app.rb
+++ app.rb
@@ -2,3 +2,24 @@ require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 require('pry')
+
+get('/') do
+  erb(:index)
+end
+
+get('/add_event') do
+  @events = Event.all()
+  erb(:add_event)
+end
+
+post('/add_event') do
+  name = params['name']
+  @category= Category.all
+  erb(:add_event)
+end
+
+post('/hangout_events') do
+  @events = Event.all()
+  @event = Event.find(params["id"])
+  redirect('/add_event')
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,5 +4,4 @@ require("spec_helper")
 describe(User) do
   it { should have_many(:details)}
   it { should have_many(:events)}
-  
 end
