795dd53ab904100f4a0eb4c9b2e283cc3a8365cc
Gabrijela Gavric, Karla Buckner, Sarah Lindmar and Sara Spink <sarah@>
2017-10-04 11:31:41 -0700

    test add user feature is passing

---
+++ spec/integration_spec.rb
@@ -0,0 +1,36 @@
+require('spec_helper')
+
+describe("adding a user", {:type => :feature}) do
+  it ("allows a user to add their name") do
+    visit("/")
+    click_link("Add a new user")
+    fill_in("name", :with => "Gabrijela")
+    click_button("Add User!")
+    expect(page).to have_content("Gabrijela")
+  end
+end
+#
+# describe("viewing the store info", {:type => :feature}) do
+#   it ("allows a user to view a store's details") do
+#     visit("/")
+#     fill_in("store name:", :with => "Imelda's Extra Wide")
+#     click_button("Submit")
+#     click_link("Imelda's Extra Wide")
+#     expect(page).to have_content("Add a brand to this store:")
+#   end
+# end
+#
+# describe("adding a brand", {:type => :feature}) do
+#   it ("allows a user to add a brand to a particular store") do
+#     visit("/")
+#     fill_in("store name:", :with => "Imelda's Extra Wide")
+#
+#     click_button("Submit")
+#
+#     click_link("Imelda's Extra Wide")
+#     fill_in("brand_name", :with => "wolky")
+#     fill_in("Enter price:", :with => 5)
+#     click_button("Add")
+#     expect(page).to have_content("Wolky")
+#   end
+# end
