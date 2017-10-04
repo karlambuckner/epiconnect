0de6662c9192479f284a742c8d174ebc2c205b45
Gabrijela Gavric, Karla Buckner, Sarah Lindmar and Sara Spink <sara@>
2017-10-04 11:50:52 -0700

    prepare to merge

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -19,16 +19,14 @@ describe("adding an event", {:type => :feature}) do
     expect(page).to have_content("BBQ")
   end
 end
-#
-# describe("viewing the store info", {:type => :feature}) do
-#   it ("allows a user to view a store's details") do
-#     visit("/")
-#     fill_in("store name:", :with => "Imelda's Extra Wide")
-#     click_button("Submit")
-#     click_link("Imelda's Extra Wide")
-#     expect(page).to have_content("Add a brand to this store:")
-#   end
-# end
+
+describe("viewing an event's details", {:type => :feature}) do
+  it ("lists the details of an event") do
+    Event.create({:title => "Epicodus Lunch Speaker", :Location => "Lunch room", :host => "Epicodus", :when => 2017-10-04, :description => "career advice" :category => "study"})
+    visit("/event/1")
+    expect(page).to have_content("Epicodus Lunch Speaker")
+  end
+end
 #
 # describe("adding a brand", {:type => :feature}) do
 #   it ("allows a user to add a brand to a particular store") do
