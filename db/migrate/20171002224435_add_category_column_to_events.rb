2474cccd83c08bc9b2dab06d7e52c0c6303a611f
Sarah Lindmar <srhcrete@gmail.com>
2017-10-04 20:21:47 -0700

    create db migration to add category to events table

--- db/migrate/20171002224435_add_category_column_to_events.rb
+++ db/migrate/20171002224435_add_category_column_to_events.rb
@@ -1,5 +1,6 @@
 class AddCategoryColumnToEvents < ActiveRecord::Migration[5.1]
   def change
     remove_column(:details, :category_id)
+    add_column(:events, :category, :string)
   end
 end
---
+++ db/migrate/20171005031432_add_column_category_to_events.rb
@@ -0,0 +1,5 @@
+class AddColumnCategoryToEvents < ActiveRecord::Migration[5.1]
+  def change
+    add_column(:events, :category, :string)
+  end
+end
