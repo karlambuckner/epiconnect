13336a7e4ca3146dc13545917edaaf421c291066
Guest User <Guest@Epicodus-5B.local>
2017-10-05 12:24:25 -0700

    comment out validations

--- lib/event.rb
+++ lib/event.rb
@@ -2,17 +2,17 @@ class Event < ActiveRecord::Base
   has_many :details
   has_many :users, through: :details
   has_many :categories, through: :details
-  validates(:title, {:presence => true, length => {:maximum => 40}})
-  validates(:title, uniqueness: { case_sensitive: false})
-  before_save(:upcase_title)
-
-  private
-
-  def upcase_title
-    array = self.title.split
-    array.each do |t|
-      t.capitalize!
-    end
-    self.title = array.join(' ')  
-  end
+  # validates(:title, {:presence => true, length => {:maximum => 40}})
+  # validates(:title, uniqueness: { case_sensitive: false})
+  # before_save(:upcase_title)
+  #
+  # private
+  #
+  # def upcase_title
+  #   array = self.title.split
+  #   array.each do |t|
+  #     t.capitalize!
+  #   end
+  #   self.title = array.join(' ')
+  # end
 end
